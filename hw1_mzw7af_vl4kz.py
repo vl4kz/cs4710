69844753583fefccebe524e9be0c60c60e244712
Maurice Wong <maurice.wong24@gmail.com>
2017-09-12 18:57:07 -0400

    Add comments for variables, add function defs

--- hw1_mzw7af_vl4kz.py
+++ hw1_mzw7af_vl4kz.py
@@ -1,11 +1,62 @@
 #!/usr/bin/python
 
-def main():
-    print("Hello")
 
-if __name__ == "__main__":
+def teachVar(argument, variable, definition):
+    '''
+    Teach <ARG> <VAR> = <STRING>
+    '''
+    pass
+
+def teachRootVar(root_var, bool_val):
+    '''
+    Teach <ROOTVAR> = <BOOL>
+    '''
+    pass
+
+def teachRule(expr, variable):
+    '''
+    Teach <EXP> -> <VAR>
+    '''
+    pass
+
+def listInst():
+    '''
+    List
+    '''
+    pass
+
+def learn():
+    '''
+    Learn
+    '''
+    pass
+
+def query(expr):
+    '''
+    Query <EXP>
+    '''
+    pass
+
+def why(expr):
+    '''
+    Why <EXP>
+    '''
+    pass
+
+def main():
+    '''
+    varDef - dict of variable definitions:
+        {string var : (string arg, string definition) ]}
+    facts - dict of variables that are true and the index of the rule that led
+        to proved it was true:
+        {string var : int index, ...}
+    rules - list of rules:
+        [(string expr, string var), ...]
+    '''
     varDef = {}
     facts = {}
     rules = []
 
+
+if __name__ == "__main__":
     main()
