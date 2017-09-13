90a64f88d573bd96239a129ba265facdad5b422a
V L <vl4kz@virginia.edu>
2017-09-12 23:33:48 -0400

    Completed parser, added testInput.txt file for piping in

--- hw1_mzw7af_vl4kz.py
+++ hw1_mzw7af_vl4kz.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import sys
 
 def teachVar(argument, variable, definition):
     '''
@@ -57,6 +57,30 @@ def main():
     facts = {}
     rules = []
 
+    for line in sys.stdin:
+        argArray = line.strip().split(" ")
 
+        if argArray[0].lower() == "teach":
+            if argArray[3] == "=":
+                stringVar = ' '.join(argArray).split(" = ")[1]
+                teachVar(argArray[1], argArray[2], stringVar)
+                #print(argArray[1] + " " + argArray[2] + " " + stringVar)
+            elif argArray[2] == "=":
+                teachRootVar(argArray[1], argArray[len(argArray)-1])
+                #print(argArray[1] + " " + argArray[len(argArray)-1])
+            else:
+                expression = ' '.join(argArray[1:]).split(" -> ")
+                teachRule(expression[0], expression[len(expression)-1])
+                #print(expression[0] + " " + expression[len(expression)-1])
+        elif argArray[0].lower() == "list":
+            listInst()
+        elif argArray[0].lower() == "learn":
+            learn()
+        elif argArray[0].lower() == "query":
+            query(argArray[len(argArray)-1])
+            #print(argArray[len(argArray)-1])
+        elif argArray[0].lower() == "why":
+            why(argArray[len(argArray)-1])
+            #print(argArray[len(argArray)-1])
 if __name__ == "__main__":
     main()
