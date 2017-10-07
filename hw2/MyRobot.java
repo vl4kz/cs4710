83a8f7cbbd79393c0ac396a4adb7d451dbf7e513
Maurice Wong <maurice.wong24@gmail.com>
2017-10-07 13:08:52 -0400

    Refactor code

--- hw2/MyRobot.java
+++ hw2/MyRobot.java
@@ -10,7 +10,7 @@ import java.lang.Math;
 
 public class MyRobot extends Robot {
     boolean isUncertain;
-    public static final double DIAG_COST = 1.0;
+    public static final double DIAG_COST = 1.4142135623;
     public static final double ADJ_COST = 1.0;
     public static Point END_POS = null;
 
@@ -26,69 +26,80 @@ public class MyRobot extends Robot {
         fnMap = new HashMap<Point, Double>();
         gnMap = new HashMap<Point, Double>();
         parentMap = new HashMap<Point, Point>();
+
+        // instance of a new PriorityQueue where the head is points with LOWER heuristic values (less costly values)
+        openQueue = initQueue();
     }
 
     @Override
     public void travelToDestination() {
+        // PLACE THE STARTING POINT INTO THE QUEUE
+        Point startingPoint = new Point(super.getX(), super.getY());
+        openQueue.add(startingPoint);
+        fnMap.put(startingPoint, diagDistance(startingPoint, END_POS));
+        gnMap.put(startingPoint, 0.0);
+
         if (isUncertain) {
-			// call function to deal with uncertainty
+            travelUncertain();
         }
         else {
-			// call function to deal with certainty
+            travelCertain();
+        }
+    }
 
-            // instance of a new PriorityQueue where the head is points with LOWER heuristic values (less costly values)
-            openQueue = initQueue();
+    public void travelUncertain() {
+        // 1. poll until distance 6 is reached
+        // 2. Move robot to best distance
+        // 3. reset the Maps DS
+        return;
+    }
 
+    public void travelCertain() {
+        while (openQueue.size() != 0) {  // while the queue is not empty
+            Point currentPoint = openQueue.poll(); // continue popping off
 
-            // PLACE THE STARTING POINT INTO THE QUEUE
-            Point startingPoint = new Point(super.getX(), super.getY());
-            openQueue.add(startingPoint);
-            fnMap.put(startingPoint, diagDistance(startingPoint, END_POS));
-            gnMap.put(startingPoint, 0.0);
+            if (currentPoint.equals(END_POS)) {
+                ArrayList<Point> path = findPath(parentMap, currentPoint);
+                moveRobot(path);
+                return;
+            }
 
-            while (openQueue.size() != 0) {  // while the queue is not empty
-                Point currentPoint = openQueue.poll(); // continue popping off
+            closedList.add(currentPoint);
 
-                if (currentPoint.equals(END_POS)) {
-                    ArrayList<Point> path = findPath(parentMap, currentPoint);
-                    moveRobot(path);
-                    return;
-                }
+            processNeighbors(currentPoint);
+        }
+    }
 
-                closedList.add(currentPoint);
-                // System.out.println(currentPoint);
+    public void processNeighbors(Point currentPoint) {
+        // iterate through all possible diagonal directions for neighbors
+        for (int rowOffset = -1; rowOffset <= 1; rowOffset++) {
+            for (int colOffset = -1; colOffset <= 1; colOffset++) {
 
-                // iterate through all possible diagonal directions for neighbors
-                for (int rowOffset = -1; rowOffset <= 1; rowOffset++) {
-                    for (int colOffset = -1; colOffset <= 1; colOffset++) {
+                Point neighbor = new Point((int) currentPoint.getX() + rowOffset, (int) currentPoint.getY() + colOffset); // create a new possible point to move to
 
-                        Point neighbor = new Point((int) currentPoint.getX() + rowOffset, (int) currentPoint.getY() + colOffset); // create a new possible point to move to
+                // System.out.println(neighbor + " " + String.valueOf(rowOffset) + " " + String.valueOf(colOffset) + " " + super.pingMap(neighbor));
 
-                        // System.out.println(neighbor + " " + String.valueOf(rowOffset) + " " + String.valueOf(colOffset) + " " + super.pingMap(neighbor));
+                if (super.pingMap(neighbor) != null && !(super.pingMap(neighbor).equals("X"))) { // check if the point is within the boundaries of the world and not a wall
+                    // System.out.println(neighbor);
 
-                        if (super.pingMap(neighbor) != null && !(super.pingMap(neighbor).equals("X"))) { // check if the point is within the boundaries of the world and not a wall
-                            // System.out.println(neighbor);
+                    if (closedList.contains(neighbor)) {
+                        continue; // this point has been already evaluated
+                    }
 
-                            if (closedList.contains(neighbor)) {
-                                continue; // this point has been already evaluated
-                            }
+                    if (!(openQueue.contains(neighbor))) { // if the neighbor is not in the openQueue
+                        openQueue.add(neighbor);
+                    }
 
-                            if (!(openQueue.contains(neighbor))) { // if the neighbor is not in the openQueue
-                                openQueue.add(neighbor);
-                            }
+                    double movementDistance = diagDistance(neighbor, currentPoint); // cost of moving from currentPoint to neighbor
 
-                            double movementDistance = diagDistance(neighbor, currentPoint); // cost of moving from currentPoint to neighbor
+                    // calculate the new g(n) by adding the old value of g(n) + cost of moving points
+                    double newgn = gnMap.getOrDefault(currentPoint, Double.POSITIVE_INFINITY) + movementDistance;
 
-                            // calculate the new g(n) by adding the old value of g(n) + cost of moving points
-                            double newgn = gnMap.getOrDefault(currentPoint, Double.POSITIVE_INFINITY) + movementDistance;
-
-                            if (newgn < gnMap.getOrDefault(neighbor, Double.POSITIVE_INFINITY)) {
-                                parentMap.put(neighbor, currentPoint);
-                                gnMap.put(neighbor, newgn);
-                                double heuristicCost = diagDistance(neighbor, END_POS);
-                                fnMap.put(neighbor, gnMap.get(neighbor) + heuristicCost);
-                            }
-                        }
+                    if (newgn < gnMap.getOrDefault(neighbor, Double.POSITIVE_INFINITY)) {
+                        parentMap.put(neighbor, currentPoint);
+                        gnMap.put(neighbor, newgn);
+                        double heuristicCost = diagDistance(neighbor, END_POS);
+                        fnMap.put(neighbor, gnMap.get(neighbor) + heuristicCost);
                     }
                 }
             }
