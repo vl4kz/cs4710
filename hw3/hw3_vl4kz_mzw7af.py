3d6eec923a1cbcb0c9f725bd9f0e4849992e7599
Maurice Wong <maurice.wong24@gmail.com>
2017-10-29 17:41:17 -0400

    value_iters with wind

--- hw3/hw3_vl4kz_mzw7af.py
+++ hw3/hw3_vl4kz_mzw7af.py
@@ -3,20 +3,25 @@ EPSILON = 0.001
 DISCOUNT = 1
 GOAL = (3, 6)
 GRID_SIZE = 7
+WIND_TYPE = 1
 
 def main():
+    for i in range(3):
+        value_iter(i)
+
+
+def value_iter(wind_type):
     util_p = initList()
     iters = 0
     delta = 0
     while True:
-        print(iters)
         iters += 1
         util = util_p
         util_p = initList()
         delta = 0
         for r in range(7):
             for c in range(7):
-                util_p[r][c] = calc_util(util, r, c)
+                util_p[r][c] = calc_util(util, r, c, wind_type)
                 new_delta = abs(util_p[r][c] - util[r][c])
                 if new_delta > delta:
                     delta = new_delta
@@ -26,22 +31,30 @@ def main():
         print(x)
 
 
-
 def initList():
     return [[0 for i in range(GRID_SIZE)] for i in range(GRID_SIZE)]
 
 
-def calc_util(util, r, c):
+def calc_util(util, r, c, wind_type):
     reward = 0 if (r, c) == GOAL else -1
-    best_action_val = get_max_action(util, r, c)
+    best_action_val = get_max_action(util, r, c, wind_type)
     return reward + DISCOUNT * best_action_val
 
 
-def get_max_action(util, r, c):
+def get_max_action(util, r, c, wind_type):
+    '''
+    wind_type: 0 = no wind, 1 = light wind, 2 = strong wind
+    '''
     best_action = float('-inf')
     for r_action in [-1, 0, 1]:
         for c_action in [-1, 0, 1]:
-            r_new = r_action + r
+            r_wind = 0
+            if wind_type == 1 and c in [3,4,5]:
+                r_wind = -1
+            elif wind_type == 2 and c in [3,4,5]:
+                r_wind = -2
+
+            r_new = r_action + r + r_wind
             c_new = c_action + c
 
             # check if out of bounds and readjust new state
